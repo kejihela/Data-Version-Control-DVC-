aa87265e68845f422e8f6cf50fe507bc599e3034
kejihela <72225471+kejihela@users.noreply.github.com>
2024-04-07 21:15:02 +0100

    second commit

---
+++ stage_01.py
@@ -0,0 +1,5 @@
+with open("artifacts.txt", "w") as f:
+    f.write("My name is keji adebayo, a computer science Msc degree holder")
+
+with open("artifacts2.txt", "w") as f:
+    f.write("I'm aspiring for my PhD")
